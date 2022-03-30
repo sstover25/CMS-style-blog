1b070e4282586808d55035a3d942a7aadc2e1780
SSto <95592688+sstover25@users.noreply.github.com>
2022-03-30 14:46:44 -0500

    added model associations

--- models/index.js
+++ models/index.js
@@ -2,4 +2,33 @@ const User = require("./User");
 const Comment = require("./Comment");
 const Post = require("./Post");
 
+// Associations
+User.hasMany(Post, {
+  foreignKey: "user_id",
+});
+
+Post.belongsTo(User, {
+  foreignKey: "user_id",
+  onDelete: "SET NULL",
+});
+
+Comment.belongsTo(User, {
+  foreignKey: "user_id",
+  onDelete: "SET NULL",
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: "post_id",
+  onDelete: "SET NULL",
+});
+
+User.hasMany(Comment, {
+  foreignKey: "user_id",
+  onDelete: "SET NULL",
+});
+
+Post.hasMany(Comment, {
+  foreignKey: "post_id",
+});
+
 module.exports = { User, Comment, Post };
